ff9f5c1d8d7deadc84265641517cc55589a22b94
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-01 19:31:29 +0300

    ft:final commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Racing_car
+# Basic Racing Game
+
+A simple Python game where you control a car and try to avoid obstacles. The game is built using the Pygame library, and your objective is to navigate your car and achieve the highest score without colliding with obstacles.
+
+## Prerequisites
+- [Python](https://www.python.org/downloads/) (>=3.6)
+- [Pygame](https://www.pygame.org/)
+
+## Installation
+1. Clone or download the repository:
+
+   ```bash
+   git clone https://github.com/Kelvin-Wepo/Racing_car
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd basic-racing-game
+   ```
+
+3. Make sure you have the required dependencies installed:
+
+   ```bash
+   pip install pygame
+   ```
+
+4. Place your car image in the project directory and name it "car.png". You can use any car image you like.
+
+## How to Play
+1. Run the game by executing the following command:
+
+   ```bash
+   python racing_game.py
+   ```
+
+2. Use the left and right arrow keys to move your car and avoid obstacles.
+
+3. The game keeps track of your score, which increases as you progress.
+
+4. The game ends when your car collides with an obstacle.
+
+5. After the game ends, a "Game Over" message and your final score will be displayed for a few seconds before the game automatically closes.
+
+## Customization
+- You can adjust the game's constants, such as the window size, car size, and speed, in the code to fit your preferences.
+- You can replace the "car.png" image with your own car image.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+Enjoy the game, and happy coding!
--- game.py
+++ game.py
@@ -14,4 +14,91 @@ BLACK = (0, 0, 0)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Basic Racing Game")
 
-#
+ Load car image
+car_image = pygame.image.load("car.png")
+car_image = pygame.transform.scale(car_image, (CAR_WIDTH, CAR_HEIGHT))
+
+# Set up the player's car
+player_x = WIDTH // 2 - CAR_WIDTH // 2
+player_y = HEIGHT - CAR_HEIGHT - 20
+player_speed = 5
+
+# Create a list of obstacles
+obstacles = []
+obstacle_speed = 5
+obstacle_width = 50
+obstacle_height = 50
+
+# Game loop
+clock = pygame.time.Clock()
+running = True
+
+score = 0
+
+def draw_player_car(x, y):
+    screen.blit(car_image, (x, y))
+
+def draw_obstacles(obstacles):
+    for obstacle in obstacles:
+        pygame.draw.rect(screen, BLACK, obstacle)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+
+    screen.fill(WHITE)
+
+    # Spawn obstacles
+    if random.randint(0, 100) < 2:
+        obstacle_x = random.randint(0, WIDTH - obstacle_width)
+        obstacle_y = 0
+        obstacles.append(pygame.Rect(obstacle_x, obstacle_y, obstacle_width, obstacle_height))
+
+    # Move obstacles
+    for obstacle in obstacles:
+        obstacle.move_ip(0, obstacle_speed)
+
+    # Remove obstacles that have gone off the screen
+    obstacles = [obstacle for obstacle in obstacles if obstacle.y < HEIGHT]
+
+    # Check for collisions
+    player_rect = pygame.Rect(player_x, player_y, CAR_WIDTH, CAR_HEIGHT)
+    for obstacle in obstacles:
+        if player_rect.colliderect(obstacle):
+            running = False
+
+    # Draw player's car
+    draw_player_car(player_x, player_y)
+
+    # Draw obstacles
+    draw_obstacles(obstacles)
+
+    # Update the display
+    pygame.display.update()
+
+    # Increase the score
+    score += 1
+
+    # Limit frame rate
+    clock.tick(60)
+
+# Game over screen
+font = pygame.font.Font(None, 36)
+game_over_text = font.render("Game Over", True, BLACK)
+score_text = font.render(f"Score: {score}", True, BLACK)
+screen.blit(game_over_text, (WIDTH // 2 - 100, HEIGHT // 2 - 50))
+screen.blit(score_text, (WIDTH // 2 - 60, HEIGHT // 2))
+pygame.display.update()
+
+# Wait for a few seconds before closing the game
+pygame.time.wait(2000)
+
+# Quit Pygame
+pygame.quit()
