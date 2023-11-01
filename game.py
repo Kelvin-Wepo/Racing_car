bece17b75fd3f7fb4a32cb53f394f9ca9b5f504f
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-01 19:29:54 +0300

    ft:this commit adds a game window

--- game.py
+++ game.py
@@ -4,3 +4,14 @@ import random
 # Initialize Pygame
 pygame.init()
 
+# Constants
+WIDTH, HEIGHT = 800, 600
+CAR_WIDTH, CAR_HEIGHT = 50, 100
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Create the game window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Basic Racing Game")
+
+#
